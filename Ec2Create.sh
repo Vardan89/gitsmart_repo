ad7c1f47dcd5d2b311a1d362e2356937ae7262c2
Ubuntu <ubuntu@ip-172-31-86-4.ec2.internal>
2024-05-16 16:15:48 +0000

    add scripts for aws

---
+++ Ec2Create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+### Create EC2 instances
+source Used_ID.sh
+EC2_ID=$(aws ec2 run-instances \
+    --image-id ami-080e1f13689e07408  \
+    --count 1 \
+    --instance-type t2.micro \
+    --key-name DevOpsEC2_smartcode1 \
+    --security-group-ids sg-0de03615473fbd902 \
+    --subnet-id $SUBNET1 \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Instans1}]" \
+    --block-device-mappings '[{"DeviceName":"/dev/sda1","Ebs":{"VolumeSize":8,"VolumeType":"gp2"}}]' \
+    --query 'Instances[0].ImageId' \
+    --output text
+)
+
+echo "$EC2_ID"
+echo "EC2_ID=$EC2_ID" >> Used_ID.sh
---
+++ createvpc.sh
@@ -0,0 +1,78 @@
+#!/bin/bash -ex
+
+# Create VPC
+CIDR_BLOCK=10.0.0.0/16
+echo "CIDR_BLOCK=10.0.0.0/16" >> Used_ID.sh
+VPC_ID=$(aws ec2 create-vpc \
+	--cidr-block $CIDR_BLOCK \
+        --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVPC}]' \
+	--query Vpc.VpcId \
+	--output text)
+echo "This is your vpc-id $VPC_ID and cidr block is $CIDR_BlOCK"
+echo "VPC_ID=$VPC_ID" >> Used_ID.sh
+# Create 2 subnet
+SIDR_BLOCKS=("10.0.1.0/24" "10.0.2.0/24")
+NUM=1
+for SUB_SIDR_BLOCK in "${SIDR_BLOCKS[@]}"
+do
+    SUBNET=$(aws ec2 create-subnet \
+	    --vpc-id $VPC_ID \
+            --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=YourSubnetName$NUM}]" \
+	    --query 'Subnet.SubnetId' \
+	    --cidr-block $SUB_SIDR_BLOCK)
+    echo "SUBNET$NUM=$SUBNET" >> Used_ID.sh
+    NUM=$((NUM + 1))
+
+done
+# Create internet gateway
+INT_GATEWAY_ID=$(aws ec2 create-internet-gateway \
+	--query 'InternetGateway.InternetGatewayId' \
+	--tag-specifications "ResourceType=internet-gateway, Tags=[{Key=Name, Value=GatewayName}]" \
+	--output text)
+echo "INT_GATEWAY_ID=$INT_GATEWAY_ID" >> Used_ID.sh
+echo "This is your internet gateway ID $INT_GATEWAY_ID"
+# Attach the internet gateway to your VPC
+echo $(aws ec2 attach-internet-gateway \
+	--vpc-id $VPC_ID \
+	--internet-gateway-id $INT_GATEWAY_ID)
+# Creat custom rout table
+ROUTE_TABLE=$(aws ec2 create-route-table \
+	--vpc-id $VPC_ID \
+	--query 'RouteTable.RouteTableId' \
+	--output text)
+echo "This is your route table ID $ROUTE_TABLE"
+echo "ROUTE_TABLE=$ROUTE_TABLE" >> Used_ID.sh
+# Create a route in the route table that points all traffic (0.0.0.0/0) to the internet gateway
+echo $(aws ec2 create-route \
+	--route-table-id $ROUTE_TABLE \
+	--destination-cidr-block 0.0.0.0/0 \
+	--gateway-id $INT_GATEWAY_ID)
+# Associate it with a subnet in your previously created VPC
+SUBNET_IDS=$(aws ec2 describe-subnets \
+	--filters "Name=vpc-id, Values=$VPC_ID" \
+	--query "Subnets[*].{ID:SubnetId}")
+ids=($(echo "$SUBNET_IDS" | jq -r '.[] | .ID'))
+
+for id in "${ids[@]}"
+do
+    echo $(aws ec2 associate-route-table \
+	    --subnet-id ${id} \
+	    --route-table-id $ROUTE_TABLE)
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
